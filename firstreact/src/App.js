4e0d6aefbb8fc869dac2441114c509726d8817a8
vijendra2244 <vs230267@gmail.com>
2023-10-06 16:28:10 +0530

    This is our first React App

---
+++ firstreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <h2 className = {"head"}>
+       Hello this is our first React App
+    </h2>
+   </>
+  );
+}
+
+export default App;
